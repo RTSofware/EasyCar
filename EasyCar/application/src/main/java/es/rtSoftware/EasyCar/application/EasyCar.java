b571f7449602d3e93ea9b7368f319880fea914c9
Fergus Pérez <Fergus1986@gmail.com>
2019-11-08 17:46:16 +0100

    Application Arquetype

---
+++ EasyCar/application/src/main/java/es/rtSoftware/EasyCar/application/EasyCar.java
@@ -0,0 +1,13 @@
+package es.rtSoftware.EasyCar.application;
+
+
+public class EasyCar {
+
+
+	public EasyCar() {
+	}
+
+	public static void main(String[] args) {
+//		SpringApplication.run(EasyCar.class, args);
+	}
+}
